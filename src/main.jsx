798940cef7d535d13746462c8995225a3db2b3e4
Sejan~ <md4sejan@gmail.com>
2023-11-27 21:24:28 +0600

    get user into manage user using tanstack

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,19 @@ import router from "./Routes/Routes";
 import ThemeProvider from "./providers/ThemeProvider";
 import { HelmetProvider } from "react-helmet-async";
 import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-      <HelmetProvider>
-        <RouterProvider router={router} />
-        <Toaster />
-      </HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <HelmetProvider>
+          <RouterProvider router={router} />
+          <Toaster />
+        </HelmetProvider>
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
--- src/pages/Dashboard/AdminDashboard/ManageUsers/ManageUsers.jsx
+++ src/pages/Dashboard/AdminDashboard/ManageUsers/ManageUsers.jsx
@@ -1,11 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "../../../../hooks/useAxiosPublic";
+import { FaTrash } from "react-icons/fa6";
+
 const ManageUsers = () => {
+  const axiosPublic = useAxiosPublic();
+
+  const { data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axiosPublic.get(`/users`);
+      return res.data;
+    },
+  });
+
   return (
     <>
       <div className="flex justify-between users-center max-w-screen-lg mx-auto p-10 bg-white rounded-t-lg">
         <h2 className="text-3xl">
           <b>Manage Users</b>
         </h2>
-        <h2 className="text-3xl">{/* <b>Total Users:</b> {users.length} */}</h2>
+        <h2 className="text-3xl">
+          <b>Total Users:</b> {users.length}
+        </h2>
       </div>
       <div className="overflow-x-auto max-w-screen-lg mx-auto bg-white p-10 rounded-b-lg">
         <table className="table">
@@ -19,7 +35,7 @@ const ManageUsers = () => {
               <th>Action</th>
             </tr>
           </thead>
-          {/* <tbody>
+          <tbody>
             {users?.map((user, index) => (
               <tr key={user._id}>
                 <th>{index + 1}</th>
@@ -28,24 +44,25 @@ const ManageUsers = () => {
                   <div className="font-bold">{user.name}</div>
                 </td>
                 <td>
-                  <h3 className="font-semibold text-xl">{user.email}</h3>
+                  <h3 className="font-semibold text-lg">{user.email}</h3>
                 </td>
                 <th>
-                  {user.role === "admin" ? (
+                  <h3 className="font-semibold text-lg">{user.role}</h3>
+                  {/* {user.role === "admin" ? (
                     "Admin"
                   ) : (
                     <button
-                      onClick={() => handleMakeAdmin(user._id)}
+                      // onClick={() => handleMakeAdmin(user._id)}
                       className="text-2xl text-[#B91C1C] hover:text-black"
                     >
                       <FaUserCheck />
                     </button>
-                  )}
+                  )} */}
                 </th>
 
                 <th>
                   <button
-                    onClick={() => handleDelete(user._id)}
+                    // onClick={() => handleDelete(user._id)}
                     className="text-2xl text-[#B91C1C] hover:text-black"
                   >
                     <FaTrash />
@@ -53,7 +70,7 @@ const ManageUsers = () => {
                 </th>
               </tr>
             ))}
-          </tbody> */}
+          </tbody>
         </table>
       </div>
     </>
